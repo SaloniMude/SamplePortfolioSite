446049baaac375c55da6631ca3c47c7848bbcb42
Saloni Mude <mude.saloni@gmail.com>
2017-05-16 02:49:50 +0530

    Completed work section
    Added images and completed work section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,7 @@
 $(function() {
   smoothScroll(300);
   workBelt();
+  workLoad();
 });
 
 // smoothScroll function is applied from the document ready function
@@ -27,5 +28,18 @@ function workBelt() {
   $('.work-return').on('click', function() {
     $('.work-belt').css('left', '0%');
     $('.work-container').hide(800);
+  });
+}
+
+function workLoad() {
+  $.ajaxSetup ({ cache: true });
+  $('.thumb-unit').click(function() {
+    var $this = $(this),
+      newTitle = $this.find('strong').text(),
+      newFolder = $this.data('folder'),
+      spinner = '<div class="loader">Loading...</div>',
+      newHTML = '/work/'+ newFolder + '.html';
+    $('.project-load').html(spinner).load(newHTML);
+    $('.project-title').text(newTitle);
   });
 }
