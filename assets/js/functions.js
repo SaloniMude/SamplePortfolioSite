13fd1128a9e44935ea78d874c2059e33595aa945
Saloni Mude <mude.saloni@gmail.com>
2017-05-15 19:40:26 +0530

    workBelt and animation
    WorkBelt and animation added. No images in expanded work

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,8 @@
+$(function() {
+  smoothScroll(300);
+  workBelt();
+});
+
 // smoothScroll function is applied from the document ready function
 function smoothScroll (duration) {
 	$('a[href^="#"]').on('click', function(event) {
@@ -11,4 +16,16 @@ function smoothScroll (duration) {
 	        }, duration);
 	    }
 	});
+}
+
+function workBelt() {
+  $('.thumb-unit').on('click', function() {
+    $('.work-belt').css('left', '-100%');
+    $('.work-container').show();
+  });
+  
+  $('.work-return').on('click', function() {
+    $('.work-belt').css('left', '0%');
+    $('.work-container').hide(800);
+  });
 }
